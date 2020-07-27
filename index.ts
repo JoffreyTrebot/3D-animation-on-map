16384de098b943f02e48e31e5b36240f3cbd7828
Joffrey <trebotjoffrey@gmail.com>
2020-07-27 16:10:03 +0200

    feat(index): create cube

--- index.ts
+++ index.ts
@@ -4,8 +4,11 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { GeoCoordinates } from "@here/harp-geoutils";
+import { GeoCoordinates,} from "@here/harp-geoutils";
 import { View } from "./View";
+import { Tile, DataSource, MapAnchor,} from "@here/harp-mapview";
+import THREE = require("three");
+import { cursorTo } from "readline";
 
 const app = new View({
     canvas: document.getElementById("map") as HTMLCanvasElement
@@ -22,7 +25,28 @@ window.addEventListener("resize", () => {
 });
 
 // center the camera to New York
-mapView.lookAt(new GeoCoordinates(40.70398928, -74.01319808), 1500, 40, 0);
+mapView.lookAt(new GeoCoordinates(49.4431, 1.0993), 1500, 40, 0);
+
+const scale = 100;
+const geometry = new THREE.BoxGeometry(1 * scale, 1 * scale, 1 * scale);
+const material = new THREE.MeshStandardMaterial({
+});
+
+function createCube(): MapAnchor<THREE.Object3D> {
+    const cube = new THREE.Object3D();
+
+    const mesh = new THREE.Mesh(geometry, material);
+    mesh.renderOrder = Number.MAX_SAFE_INTEGER;
+    cube.add(mesh);
+    return cube;
+}
+
+const geoPosition = mapView.getGeoCoordinatesAt(49.4431, 1.0993);
+geoPosition.altitude = 50;
+
+const cube = createCube();
+cube.anchor = geoPosition;
+mapView.mapAnchors.add(cube);
 
 // make sure the map is rendered
 mapView.update();
