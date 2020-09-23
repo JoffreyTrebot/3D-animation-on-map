7ddade9e66601feeceb99007263e858438b97d8f
Joffrey Trebot <trebotjoffrey@gmail.com>
2020-09-23 16:22:33 +0200

    feat(index): track animation

--- cube.ts
+++ cube.ts
@@ -1,8 +1,9 @@
+import { GeoCoordinates } from '@here/harp-geoutils';
 import { MapAnchor } from "@here/harp-mapview";
 import THREE = require("three");
 
 export function createCube(): MapAnchor<THREE.Object3D> {
-    const scale = 100;
+    const scale = 100000;
     const geometry = new THREE.BoxGeometry(1 * scale, 1 * scale, 1 * scale);
     const material = new THREE.MeshStandardMaterial({});
 
@@ -16,7 +17,7 @@ export function createCube(): MapAnchor<THREE.Object3D> {
 
 
 export function randomCubePosition(mapView) {
-    const geoPosition = mapView.getGeoCoordinatesAt(Math.floor(Math.random() * 49) + 1, Math.floor(Math.random() * 49) + 1);
+    const geoPosition = new GeoCoordinates(34.59704151614417, -22.8515625)
     geoPosition.altitude = 50;
 
     const cube = createCube();
